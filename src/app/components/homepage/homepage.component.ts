4c524966ce010a79e24ae84f55c1d4ab7728ebde
federico.villani <federico.villani@hotmail.com>
2019-02-18 10:52:05 +0100

    fix homepage

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,21 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-    /**
-     * endpoint httpcall
-     *
-     * http://localhost:3500/cart - GET
-     * http://localhost:3500/saveCart - POST
-     */
-  }
-
-}
+export class HomepageComponent { }
